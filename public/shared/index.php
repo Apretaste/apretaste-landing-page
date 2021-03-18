83f2f327715c4cd47320b0083c73102dc9b5a853
kumahacker <kuma@apretaste.org>
2021-03-18 16:26:20 -0300

    update

---
+++ public/shared/index.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../../vendor/autoload.php";
+define ('PACKAGES', __DIR__);
+
+use divengine\ways;
+use divengine\div;
+use Apretaste\Database;
+use Apretaste\Person;
+
+ways::listen("/profile/{hash}", function($data, $args) {
+
+	$profile = Database::queryFirst("SELECT id FROM person WHETE hash = '{$args['hash']}';");
+	$person = Person::find($profile->id);
+
+	echo new div('profile', [
+		'profile' => $person
+	]);
+});
+
+ways::bootstrap();
---
+++ public/shared/profile.tpl
@@ -0,0 +1,10 @@
+{% layout %}
+{{body
+
+<div style="margin:auto;">
+    <img src="{$profile.picture}" />
+    <p>@{$person.username}</p>
+    <p>{$profile.aboutMe}</p>
+</div>
+
+body}}
