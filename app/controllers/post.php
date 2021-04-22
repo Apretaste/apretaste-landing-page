7be830e181baf1e283cb22e69672e18afe736707
Salvi Pascual <salvi.pascual@gmail.com>
2021-04-22 10:28:03 -0400

    fix issue

--- app/controllers/post.php
+++ app/controllers/post.php
@@ -49,7 +49,7 @@ class Post
 
 		// send data to the view
 		$this->view->data->title = "Nota de @{$note->username}";
-		$this->view->data->description = $note->text;
+		$this->view->data->description = str_replace(["\r","\n",'"'], '', $note->text);
 		$this->view->data->keywords = "apretaste,cuba,red social cuba,chat cuba,apretaste cuba,chatear con amigos,buscar amigos por internet,buscar gente,hacer amigos online,instagram en cuba,redes sociales para conocer gente,facebook cuba,facebook en cuba,paginas para conocer gente gratis,amistades por internet,twitter en cuba,twitter cuba,instagram para cuba,paginas para conocer gente,hacer amigos por internet,quiero conocer gente,chat conocer gente,twitter para cuba,buscar personas para amistad,paginas conocer gente,chatear con gente,paginas para conseguir amigos,amigos en cuba,apps para conocer gente,hacer amigos en cuba,red social cubanos,conocer cubanos,conocer personas,amigos por internet,red social cuba,paginas para hacer amigos,como conocer amigos,hacer amigos,paginas de amistad,redes sociales para hacer amigos,instagram cuba,facebook para cuba";
 		$this->view->data->feature = empty($note->image) ? false : $note->image;
 		$this->view->data->note = $note;
